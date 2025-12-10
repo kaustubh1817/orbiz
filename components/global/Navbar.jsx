de3d2ac9a40e3749eb31bf4efd7af1c4cabc274d
Akshay Karada <akshaykarada72.o@gmail.com>
2025-12-10 21:54:56 +0530

    update the footer 10% work remaining

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -42,8 +42,8 @@ export default function Navbar() {
                     <Image
                         src="/orbiz_logo.png"
                         alt="Orbiz"
-                        width={120}
-                        height={60}
+                        width={100}
+                        height={50}
                     />
                 </Link>
             </div>
