4da4bf0841073466640141c469bf854bc4c11803
Akshay Karada <akshaykarada72.o@gmail.com>
2025-12-12 20:17:41 +0530

    hero mobile view and images update completed

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -7,39 +7,59 @@ const imageSets = [
       src: "/landing/hero1.webp",
       alt: "mid-box-left-image", 
       className:
-        "absolute 3xl:-top-15.5 md:-top-13 top-84.5 right-8 3xl:right-29 md:right-25.5 3xl:h-[520px] md:h-[420px] h-[350px]",
+        "absolute 3xl:-top-15.5 md:-top-13 -top-19.5 right-0 3xl:right-29 md:right-25.5 3xl:h-[520px] md:h-[420px] h-[368px]",
     },
     {
       src: "/landing/hero2.webp",
       alt: "mid-box-right-image",
       className:
-        "absolute 3xl:top-62 md:top-50 top-137 right-36 3xl:right-70 md:right-59 3xl:h-[500px] md:h-[400px] h-[340px]",
+        "absolute 3xl:top-62 md:top-50 top-36 right-28.5 3xl:right-70 md:right-59 3xl:h-[500px] md:h-[400px] h-[360px]",
     },
     {
       src: "/landing/hero3.webp",
       alt: "mid-box-image",
       className:
-        "absolute right-0 top-103 md:-top-17.5 3xl:h-[705px] md:h-[582px] h-[400px]",
+        "hidden md:block absolute right-0 top-103 md:-top-17.5 3xl:h-[705px] md:h-[582px] h-[400px]",
     },
   ],
-   [
+  [
     {
-      src: "/landing/hero1.webp",
+      src: "/landing/hero4.webp",
       alt: "mid-box-left-image", 
       className:
-        "absolute 3xl:-top-15.5 md:-top-13 top-84.5 right-8 3xl:right-29 md:right-25.5 3xl:h-[520px] md:h-[420px] h-[350px]",
+        "absolute 3xl:-top-15.5 md:-top-13 -top-19.5 right-0 3xl:right-29 md:right-25.5 3xl:h-[520px] md:h-[420px] h-[368px]",
     },
     {
-      src: "/landing/hero2.webp",
+      src: "/landing/hero5.webp",
       alt: "mid-box-right-image",
       className:
-        "absolute 3xl:top-62 md:top-50 top-137 right-36 3xl:right-70 md:right-59 3xl:h-[500px] md:h-[400px] h-[340px]",
+        "absolute 3xl:top-62 md:top-50 top-36 right-28.5 3xl:right-70 md:right-59 3xl:h-[500px] md:h-[400px] h-[360px]",
     },
     {
-      src: "/landing/hero3.webp",
+      src: "/landing/hero6.webp",
       alt: "mid-box-image",
       className:
-        "absolute right-0 top-103 md:-top-17.5 3xl:h-[705px] md:h-[582px] h-[400px]",
+        "hidden md:block absolute right-0 top-103 md:-top-17.5 3xl:h-[705px] md:h-[582px] h-[400px]",
+    },
+  ],
+  [
+    {
+      src: "/landing/hero7.webp",
+      alt: "mid-box-left-image", 
+      className:
+        "absolute 3xl:-top-15.5 md:-top-13 -top-19.5 right-0 3xl:right-29 md:right-25.5 3xl:h-[520px] md:h-[420px] h-[368px]",
+    },
+    {
+      src: "/landing/hero8.webp",
+      alt: "mid-box-right-image",
+      className:
+        "absolute 3xl:top-62 md:top-50 top-36 right-28.5 3xl:right-70 md:right-59 3xl:h-[500px] md:h-[400px] h-[360px]",
+    },
+    {
+      src: "/landing/hero9.webp",
+      alt: "mid-box-image",
+      className:
+        "hidden md:block absolute right-0 top-103 md:-top-17.5 3xl:h-[705px] md:h-[582px] h-[400px]",
     },
   ],
 ];
@@ -61,7 +81,7 @@ export default function Hero() {
   }, []);
 
   return (
-    <section className="pl-10 md:pl-18 2xl:pl-18 3xl:pl-22 relative">
+    <section className="pl-10 md:pl-18 2xl:pl-18 3xl:pl-22 relative py-10 md:py-0 3xl:py-0">
       <div className="flex flex-col md:flex-row w-full text-center md:text-left gap-10">
         <div className="flex flex-col w-full  md:w-1/2 justify-center gap-10 pr-10 md:pr-0">
           <h1 className="fl1">
@@ -79,11 +99,11 @@ export default function Hero() {
           </a>
         </div>
 
-        <div className="relative flex flex-col w-full md:w-1/2 md:h-[625px] 3xl:h-[750px] h-[500px]">
+        <div className="relative flex flex-col w-full md:w-1/2 md:h-[625px] 3xl:h-[750px] h-[445px]">
           <img
             src="/landing/top-box.png"
             alt="top-box-image"
-            className="absolute 3xl:top-12 md:top-10 top-103 right-56 3xl:right-98.5 md:right-82.5 md:mb-9 md:h-17 md:w-42 h-9 w-21"
+            className="absolute 3xl:top-12 md:top-10 top-0 right-52 3xl:right-98.5 md:right-82.5 md:mb-9 md:h-17 md:w-42 h-18 w-42"
           />
           {imageSets[currentSet].map((img, idx) => (
             <img
@@ -98,7 +118,7 @@ export default function Hero() {
           <img
             src="/landing/bottom-box.svg"
             alt="bottom-box-image"
-            className="absolute bottom-17.5 md:-bottom-0 3xl:bottom-0 right-0 md:right-0 md:mb-9 md:h-17 md:w-42 h-9 w-21"
+            className="absolute bottom-5 md:-bottom-0 3xl:bottom-0 right-2 md:right-0 md:mb-9 md:h-17 md:w-42 h-18 w-58"
           />
         </div>
          {/* <a href="/services" className="md:hidden block red-btn mt-5 md:mt-10 mx-auto md:mx-0">
