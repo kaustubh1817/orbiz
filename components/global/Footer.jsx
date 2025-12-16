18f7c17d5f881e35467f380d3c99aceb447a65b6
Akshay Karada <akshaykarada72.o@gmail.com>
2025-12-16 18:58:47 +0530

    update the footer and aligment and the ipad correction

--- components/global/Footer.jsx
+++ components/global/Footer.jsx
@@ -123,7 +123,7 @@ export default function Footer() {
               ))}
             </div>
           </div>
-          <div className="flex flex-row justify-around gap-3 md:gap-57">
+          <div className="flex flex-row justify-between  gap-12 lg:gap-16 xl:gap-40">
             <div className="flex flex-col gap-3 justify-center">
               <h4 className="fl5 mb-0 md:mb-4">Company</h4>
               <ul className="space-y-2">
