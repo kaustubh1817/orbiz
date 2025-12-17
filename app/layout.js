bb7d3cd5ee02b3e82892abe9d50cf9754b602826
Akshay Karada <akshaykarada72.o@gmail.com>
2025-12-17 15:23:32 +0530

    update the animation of hero . and remove geist from layou

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/global/Navbar.jsx";
 import Footer from "../components/global/Footer.jsx";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export default function RootLayout({ children }) {
   return (
